d1815b552dc8d055b3d8a41697a419afe7d0eab4
Faith Chepngeno <faithchepngeno850@gmail.com>
2025-11-13 02:20:55 +0300

    Django app

---
+++ employees/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from employees.models import Employee
+
+# Create your views here.
+def employee_detail(request,pk):
+    employee=get_object_or_404(Employee,pk=pk)
+    return HttpResponse(employee)
