5240952e733d717498014cfd8be34296e048c21b
Faith Chepngeno <faithchepngeno850@gmail.com>
2025-11-16 02:17:43 +0300

    views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 def list_books(request):
     books=Book.objects.all()
-    return render(request,"list_books.html",{"books":books})
+    return render(request,"relationship_app/list_books.html",{"books":books})
 
 class LibraryDetailView(DetailView):
     model = Library
