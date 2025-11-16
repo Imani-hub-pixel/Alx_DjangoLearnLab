c88e77d6333c4f73f324afb421ba20664d0f9001
Faith Chepngeno <faithchepngeno850@gmail.com>
2025-11-16 13:35:47 +0300

    detailview

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from .models import  Book
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
+
 from .models import Library
 
 def list_books(request):
