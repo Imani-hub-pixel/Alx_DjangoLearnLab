c81c3877c79df51be4a51fc3e53e8018b683d007
Faith Chepngeno <faithchepngeno850@gmail.com>
2025-11-16 15:55:40 +0300

    urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,11 +14,11 @@ urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
 
-    path('books/add/', add_book, name='add_book'),
-    path('books/edit/<int:pk>/', edit_book, name='edit_book'),
+    path('books/add_book/', add_book, name='add_book'),
+    path('books/edit_book/<int:pk>/', edit_book, name='edit_book'),
     path('books/delete/<int:pk>/', delete_book, name='delete_book'),
 
-    
+
     path("login/", CustomLoginView.as_view(template_name="login.html"), name="login"),
     path("logout/",CustomLogoutView.as_view(template_name="logout.html"), name="logout"),
     path("register/", views.register, name="register"),
