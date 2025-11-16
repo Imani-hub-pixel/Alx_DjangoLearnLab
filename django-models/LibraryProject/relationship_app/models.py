daabc8ba64cfc81ef64c8fdea7381a3925327fc5
Faith Chepngeno <faithchepngeno850@gmail.com>
2025-11-16 15:50:01 +0300

    implementing cutom permissions

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -12,8 +12,17 @@ class Book(models.Model):
     title=models.CharField(max_length=200)
     author=models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
+
+
     def __str__(self):
         return self.title
+
+    class Meta:
+        permissions=[
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
 
 class Library(models.Model):
     name=models.CharField(max_length=150)
