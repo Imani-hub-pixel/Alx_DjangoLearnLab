0ded3c4e7968ce9ec3aad9548ce5b836830f2059
Faith Chepngeno <faithchepngeno850@gmail.com>
2025-11-23 00:56:04 +0300

    register models

--- advanced_features_and_security/LibraryProject/accounts/admin.py
+++ advanced_features_and_security/LibraryProject/accounts/admin.py
@@ -1,33 +1,4 @@
 from django.contrib import admin
 
 # Register your models here.
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
-from django.forms import TextInput, Textarea
-from django import forms
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-
-    # fields shown in admin list
-    list_display = ("email", "date_of_birth", "is_staff", "is_active")
-    list_filter = ("is_staff", "is_active")
-
-    # fields for creating/editing users
-    fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        ("Personal Info", {"fields": ("date_of_birth", "profile_photo")}),
-        ("Permissions", {"fields": ("is_staff", "is_superuser", "is_active", "groups", "user_permissions")}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("email", "password1", "password2", "is_staff", "is_active"),
-        }),
-    )
-
-    search_fields = ("email",)
-    ordering = ("email",)
-
-admin.site.register(CustomUser, CustomUserAdmin)
