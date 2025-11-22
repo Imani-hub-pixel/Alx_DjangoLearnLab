c630cd9efbddb79536f644570a94f40987e5c7c9
Faith Chepngeno <faithchepngeno850@gmail.com>
2025-11-23 01:32:59 +0300

    implement permission and group system

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,67 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import BookForm  # assuming you have a ModelForm for Book
+
+# View a book (requires can_view)
+@permission_required('bookshelf.can_view', raise_exception=True)
+def view_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'bookshelf/book_detail.html', {'book': book})
+
+# Create a book (requires can_create)
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/book_form.html', {'form': form})
+
+# Edit a book (requires can_edit)
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'bookshelf/book_form.html', {'form': form})
+
+# Delete a book (requires can_delete)
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})
+
+"""
+Permissions Setup:
+
+Book model has custom permissions:
+- can_view: Allows viewing book details
+- can_create: Allows creating new books
+- can_edit: Allows editing books
+- can_delete: Allows deleting books
+
+Groups:
+- Viewers: can_view
+- Editors: can_create, can_edit
+- Admins: can_view, can_create, can_edit, can_delete
+
+Usage:
+- @permission_required('bookshelf.can_edit', raise_exception=True) protects edit views
+- Users must be assigned to appropriate group to access features
+"""
