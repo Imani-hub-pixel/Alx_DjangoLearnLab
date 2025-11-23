0f773e2de28e75c96ef04f9f26c5a978566ae880
Faith Chepngeno <faithchepngeno850@gmail.com>
2025-11-23 18:04:27 +0300

    security

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,14 +1,25 @@
 # bookshelf/forms.py
 from django import forms
 
+# Form used in form_example.html
+class ExampleForm(forms.Form):
+    name = forms.CharField(
+        max_length=100,
+        required=True,
+        help_text="Enter your name"
+    )
+    message = forms.CharField(
+        widget=forms.Textarea,
+        required=True,
+        help_text="Enter your message"
+    )
+
+
+# Search form for book_list.html
 class BookSearchForm(forms.Form):
     q = forms.CharField(
         required=False,
         max_length=100,
         strip=True,
-        help_text="Search by title or author (max 100 chars)."
+        help_text="Search by title or author (max 100 chars)"
     )
-
-    def clean_q(self):
-        q = self.cleaned_data.get("q", "")
-        return q
