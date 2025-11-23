38699c9f8f40425c7650fef9f0079c94c105c553
Faith Chepngeno <faithchepngeno850@gmail.com>
2025-11-23 18:57:58 +0300

    token authentication

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,12 +4,22 @@ from django.shortcuts import render
 from rest_framework import generics,viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import permissions
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    Provides full CRUD operations for Book model.
+    Only authenticated users can access endpoints.
+    Authentication method: TokenAuthentication
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # only logged-in users
+
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+
 
