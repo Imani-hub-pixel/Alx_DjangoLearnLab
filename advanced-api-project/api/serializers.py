8c8a4a65a29efd5cde8e3724eb00aefcbad1a36d
Faith Chepngeno <faithchepngeno850@gmail.com>
2025-12-07 18:54:26 +0300

    serializer validation

---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,20 @@
+from .models import Book,Author
+from  rest_framework import serializers
+from datetime import datetime
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Book
+        fields=["__all__"]
+    def validate(self,attrs):
+        current_year=datetime.now().year
+        pub_year=attrs.get("publication_year")
+
+
+        if pub_year and pub_year > current_year:
+            raise serializers.ValidationError(
+                {"publication_year":"publication year cannnot be in the future"}
+            )
+        
+        return attrs
+
