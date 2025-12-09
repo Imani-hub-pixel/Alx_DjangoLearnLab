062a2f4987f4b159c6cf53260974bb08e2054b0a
Faith Chepngeno <faithchepngeno850@gmail.com>
2025-12-09 21:36:45 +0300

    user authentication views,templates and url patterns

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm
+from django.contrib import messages
 
 # Create your views here.
+
+def register_view(request):
+    if request.method=="POST":
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            login(request,user)
+            messages.success(request,"Registration successful")
+            return redirect("profile")
+        else:
+            form=RegisterForm()
+        return render(request,"register.html",{"form":form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+@login_required
+def profile_view(request):
+    return render(request, "profile.html")
+
+@login_required
+def edit_profile_view(request):
+    user = request.user
+
+    if request.method == "POST":
+        user.username = request.POST.get("username")
+        user.email = request.POST.get("email")
+        user.save()
+        messages.success(request, "Profile updated successfully")
+        return redirect("profile")
+
+    return render(request, "edit_profile.html", {"user": user})
