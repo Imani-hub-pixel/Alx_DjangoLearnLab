bea0304d7f826781c8a3448eaebe7bb7878bf786
Faith Chepngeno <faithchepngeno850@gmail.com>
2025-12-09 19:30:22 +0300

    user,port

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = "django_blog.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',      
+        'PASSWORD': '', 
+        'HOST': '',      
+        'PORT': '',      
     }
 }
+
 
 
 # Password validation
