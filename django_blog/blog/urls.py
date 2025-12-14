8d88d55b6b1ee44fef4ce52dc0946bd7f4c9cdba
Faith Chepngeno <faithchepngeno850@gmail.com>
2025-12-14 13:56:15 +0300

    Developed and integrated features to CRUD blog posts

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
-from . import views
+from .views import (
+    PostListView, PostDetailView, PostCreateView,
+    PostUpdateView, PostDeleteView,
+    login_view, logout_view, register_view, profile_view, edit_profile_view,home_view
+)
 
 urlpatterns = [
-    path("login/", views.login_view, name="login"),
-	path("logout/", views.logout_view, name="logout"),
-    path("register/", views.register_view, name="register"),
-    path("profile/", views.profile_view, name="profile"),
-    path("profile/edit/", views.edit_profile_view, name="edit_profile"),
+
+
+    path("", PostListView.as_view(), name="posts"),
+    path("posts/new/", PostCreateView.as_view(), name="post_create"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post_edit"),
+    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
+
+    path("",home_view, name="home"),
+    path("login/",login_view, name="login"),
+	path("logout/",logout_view, name="logout"),
+    path("register/",register_view, name="register"),
+    path("profile/",profile_view, name="profile"),
+    path("profile/edit/",edit_profile_view, name="edit_profile"),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import login,authenticate,logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from .forms import RegisterForm
 from django.contrib import messages
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
+from django.shortcuts import render
+
+def home_view(request):
+    return render(request, "blog/home.html")
+
 
 def register_view(request):
     if request.method=="POST":
@@ -15,9 +23,9 @@ def register_view(request):
             login(request,user)
             messages.success(request,"Registration successful")
             return redirect("profile")
-        else:
-            form=RegisterForm()
-        return render(request,"register.html",{"form":form})
+    else:
+        form=RegisterForm()
+    return render(request,"blog/register.html",{"form":form})
 
 def login_view(request):
     if request.method == "POST":
@@ -29,7 +37,7 @@ def login_view(request):
     else:
         form = AuthenticationForm()
 
-    return render(request, "login.html", {"form": form})
+    return render(request, "blog/login.html", {"form": form})
 
 def logout_view(request):
     logout(request)
@@ -37,7 +45,7 @@ def logout_view(request):
 
 @login_required
 def profile_view(request):
-    return render(request, "profile.html")
+    return render(request, "blog/profile.html")
 
 @login_required
 def edit_profile_view(request):
@@ -50,4 +58,53 @@ def edit_profile_view(request):
         messages.success(request, "Profile updated successfully")
         return redirect("profile")
 
-    return render(request, "edit_profile.html", {"user": user})
+    return render(request, "blog/edit_profile.html", {"user": user})
+
+class PostListView(ListView):
+    model=Post
+    template_name="blog/post_list.html"
+    context_object_name="posts"
+    ordering=["-published_date"]
+    paginate_by=5
+
+class PostDetailView(DetailView):
+    model=Post
+    template_name="blog/post_detail.html"
+    context_object_name="post"
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model=Post
+    template_name="blog/post_form.html"
+    fields=["title","content"]
+
+    def form_valid(self,form):
+        form.instance.author=self.request.user
+        messages.success(self.request, "Post created successfully")
+        return super().form_valid(form)
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.pk})
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model=Post
+    template_name="blog/post_form.html"
+    fields=["title","content"]
+
+    def form_valid(self,form):
+        form.instance.author=self.request.user
+        messages.success(self.request, "Post updated successfully")
+        return super().form_valid(form)
+
+    def test_func(self):
+        post=self.get_object()
+        if self.request.user==post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("post_list")
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
