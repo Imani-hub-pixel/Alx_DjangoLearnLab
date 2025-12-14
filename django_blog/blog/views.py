ea75751bbd01be30df6a7b246dba0cbfe47dac5f
Faith Chepngeno <faithchepngeno850@gmail.com>
2025-12-14 15:07:25 +0300

    comment urls

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -129,12 +129,9 @@ class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     form_class = CommentForm
     template_name = "blog/comment_form.html"
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        from .models import Post
-        post_id = self.kwargs.get("post_id")
-        form.instance.post = Post.objects.get(pk=post_id)
-        return super().form_valid(form)
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
 
     def get_success_url(self):
         return reverse_lazy("post_detail", kwargs={"pk": self.object.post.pk})
