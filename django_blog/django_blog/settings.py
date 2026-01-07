564e8e3e7e6c542fcb833a4247082568fff7de00
Faith Chepngeno <faithchepngeno850@gmail.com>
2026-01-07 17:48:21 +0300

    delpoy

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%(3z5@zmn!-c!c251yj^1nrt3u)j=xehir7d919_isy&f^z#d&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://alx-djangolearnlab-2-1gmk.onrender.com","localhost", "127.0.0.1"]
 
 
 # Application definition
