edfda744f6aa725bb13b72157e50c30f22aae2e8
Faith Chepngeno <faithchepngeno850@gmail.com>
2026-01-07 17:59:17 +0300

    Fix STATIC_ROOT for deployment

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -120,10 +120,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+import os
 
-STATIC_URL = "static/"
-STATICFILES_DIRS=[
-    BASE_DIR/"static"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = "/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  
 ]
 
 # Default primary key field type
